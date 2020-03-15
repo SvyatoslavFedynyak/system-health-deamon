b3ba290a593691346d16908b44a6d8eed61cee50
Svyatoslav <svyatoslav912@gmail.com>
2020-03-15 14:39:34 +0200

    Confing dependency added

--- source/main.py
+++ source/main.py
@@ -1,24 +1,52 @@
 import modules.deamon as daemon
+import argparse
 import sys
 import os
 
+def load_config(path):
+    '''Loads config from file'''
+    config_dict = {}
+    with open(path, 'r') as fp:
+        for line in fp.read().splitlines():
+            line = line.rstrip()
+            if line != '':
+                if line[0] != '#':
+                    pair = line.split('=')
+                    config_dict[pair[0]] = pair[1]
+    return config_dict
+
 if __name__ == '__main__':
-    env_path = os.path.abspath('.')
 
-    if len(sys.argv) < 2:
-        print("Usage: {0} start|stop|restart".format(sys.argv[0]))
-        sys.exit(2)
+    env_path = os.path.abspath(os.path.dirname(''))
 
-    daemon = daemon.CollectorDaemon(pidfile= env_path + '/tmp/daemon.pid',
+    # Argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("command", help="command, given to daemon. Possible options: start|stop|restart", type=str)
+    parser.add_argument("-c", "--config", help="config file path", type=str)
+    args = parser.parse_args()
+
+    # Get app paths and parse config file
+    if args.config:
+        config_path = args.config
+    else:
+        config_path = os.path.abspath(os.path.join(env_path, '..', 'config', 'daemon.cfg'))
+    config = load_config(config_path)
+
+    root_dir = config['root_dir']
+    pid_path = os.path.abspath(os.path.join(root_dir, config['pid_path']))
+    output_path = os.path.abspath(os.path.join(root_dir, config['output_log']))
+    error_path = os.path.abspath(os.path.join(root_dir, config['error_log']))
+
+    daemon = daemon.CollectorDaemon(pidfile=pid_path,
                                     stdin='/dev/null',
-                                    stdout= env_path + '/tmp/output.log',
-                                    stderr= env_path + '/tmp/error.log')
-    if sys.argv[1] == 'start':
+                                    stdout=output_path,
+                                    stderr=error_path)
+    if args.command == 'start':
         daemon.start()
-    elif sys.argv[1] == 'stop':
+    elif args.command == 'stop':
         daemon.stop()
-    elif sys.argv[1] == 'restart':
+    elif args.command == 'restart':
         daemon.restart()
     else:
-        print("Unknown command '{0}'".format(sys.argv[1]))
+        print(args.help)
         sys.exit(2)
