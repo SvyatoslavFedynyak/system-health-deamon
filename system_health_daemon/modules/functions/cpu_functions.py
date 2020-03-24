f88fbad9de7cbf14750257017a2ab69ecd1ae4c0
SvyatoslavFedynyak <svyatoslav912@gmail.com>
2020-03-24 15:48:11 +0200

    Simplified function res to json convertation

--- system_health_daemon/modules/functions/cpu_functions.py
+++ system_health_daemon/modules/functions/cpu_functions.py
@@ -4,6 +4,7 @@ import datetime
 
 
 def CPU_currentTimes():
+    '''Return current CPU time values'''
     raw = psutil.cpu_times(True)
     raw_result = {}
     cpu_dict = {}
@@ -11,16 +12,8 @@ def CPU_currentTimes():
     raw_result['datetime'] = str(datetime.datetime.now())
     raw_result['data'] = []
     for idx, cpu in enumerate(raw):
-        cpu_dict['CPU'] = str(idx)
-        cpu_dict['user'] = str(cpu.user)
-        cpu_dict['system'] = str(cpu.system)
-        cpu_dict['nice'] = str(cpu.nice)
-        cpu_dict['idle'] = str(cpu.idle)
-        cpu_dict['iowait'] = str(cpu.iowait)
-        cpu_dict['irq'] = str(cpu.irq)
-        cpu_dict['softirq'] = str(cpu.softirq)
-        cpu_dict['steal'] = str(cpu.steal)
-        cpu_dict['guest'] = str(cpu.guest)
-        cpu_dict['guest_nice'] = str(cpu.guest_nice)
+        cpu_dict = raw[idx]._asdict()
         raw_result['data'].append(cpu_dict.copy())
     return json.dumps(raw_result, indent='\t')
+
+
